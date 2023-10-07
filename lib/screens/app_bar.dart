24b7da5b58e039d0c8f0b6dae6957b2f478ac451
eycis <dzugam00@jcu.cz>
2023-10-07 18:42:59 +0200

    update

--- lib/screens/app_bar.dart
+++ lib/screens/app_bar.dart
@@ -2,11 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:todo/screens/styles.dart';
 import 'package:todo/screens/main_screen.dart';
 
-class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
+  const CustomAppBar(
+      {super.key, required this.listCount, required this.onListCountUpdated});
   final int listCount;
+  final Function(int) onListCountUpdated;
 
-  CustomAppBar({Key? key, required this.listCount});
+  @override
+  State<CustomAppBar> createState() => _CustomAppBarState();
 
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class _CustomAppBarState extends State<CustomAppBar> {
   @override
   Widget build(BuildContext context) {
     return AppBar(
@@ -19,24 +28,38 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
         ),
       ),
       actions: [
-        // ...
-        IconButton(
-          icon: const Icon(
-            Icons.add,
+        Container(
+          padding: const EdgeInsets.only(left: 16),
+          child: const Icon(
+            Icons.menu,
             color: white,
             size: 30,
           ),
-          onPressed: () {
-            // Použití listCount
-            setState(() {
-              listCount = listCount + 1; // Aktualizace listCount
-            });
-          },
         ),
+        const Spacer(),
+        Container(
+          padding: const EdgeInsets.only(top: 8),
+          child: const Text(
+            'ToDo List',
+            style: TextStyle(
+              color: white,
+              fontSize: 24,
+            ),
+          ),
+        ),
+        const Spacer(),
+        Container(
+            padding: const EdgeInsets.only(right: 16),
+            child: IconButton(
+                icon: const Icon(
+                  Icons.add,
+                  color: white,
+                  size: 30,
+                ),
+                onPressed: () {
+                  widget.onListCountUpdated(widget.listCount + 1);
+                })),
       ],
     );
   }
-
-  @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -2,31 +2,43 @@ import 'package:flutter/material.dart';
 //import 'package:todo/model/todo.dart';
 import 'package:todo/screens/app_bar.dart';
 //import 'package:todo/screens/main_todo_list.dart';
-import 'package:todo/screens/search_box.dart';
+//import 'package:todo/screens/search_box.dart';
 import 'package:todo/screens/styles.dart';
 
 class MainScreen extends StatefulWidget {
+  MainScreen({Key? key}) : super(key: key);
+
   @override
-  _MainScreenState createState() => _MainScreenState();
+  State<MainScreen> createState() => _MainScreenState();
 }
 
 class _MainScreenState extends State<MainScreen> {
   int listCount = 1;
 
+  void _incrementListCount(int newCount) {
+    setState(() {
+      listCount = newCount + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: black,
-      appBar: CustomAppBar(listCount: listCount),
+      appBar: CustomAppBar(
+        listCount: listCount,
+        onListCountUpdated: _incrementListCount,
+      ),
       body: ListView.builder(
           shrinkWrap: true,
           itemBuilder: (BuildContext context, int index) {
             return Container(
-                color: grey,
-                child: ListTile(
-                  trailing: Icon(Icons.add),
-                  title: Text('note'),
-                ));
+              color: grey,
+              child: ListTile(
+                trailing: Icon(Icons.add),
+                title: Text('note'),
+              ),
+            );
           },
           itemCount: listCount),
     );
